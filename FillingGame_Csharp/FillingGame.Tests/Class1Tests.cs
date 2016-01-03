ca3f6eaeae4af557e5caf7195e68b0d492f07338
Maximilian Lackaw <mlackaw@gmail.com>
2016-01-03 19:04:23 +0100

    Test setup

---
+++ FillingGame_Csharp/FillingGame.Tests/Class1Tests.cs
@@ -0,0 +1,20 @@
+﻿using NUnit.Framework;
+
+using FluentAssertions;
+
+namespace FillingGame.Tests
+{
+    [TestFixture]
+    public class Class1Tests
+    {
+        [Test]
+        public void FirstTest()
+        {
+            var c = new Class1();
+
+            var result = c.Add(5, 2);
+
+            Assert.AreEqual(result, 7);
+        }
+    }
+}
